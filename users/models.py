a3bb0848b417c3cea9751425875cb5637550c1ab
mamarezak22 <mohammadreza13842018@gmail.com>
2025-03-08 16:48:26 +0330

    add userhistory model

--- users/models.py
+++ users/models.py
@@ -2,3 +2,16 @@ from django.db import models
 
 class TelegramUser(models.Model):
     numeric_id = models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return self.numeric_id
+
+class UserHistory(models.Model):
+    user = models.ForeignKey("TelegramUser", on_delete=models.CASCADE)
+    count_of_won_games = models.IntegerField(default=0)
+    count_of_lose_games = models.IntegerField(default=0)
+    count_of_correct_answer = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'history of {self.user}'
+
