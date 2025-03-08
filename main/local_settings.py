a2a5a5a3cafc24690089adf3ac967eb968c638f4
mamarezak22 <mohammadreza13842018@gmail.com>
2025-03-08 17:13:36 +0330

    add game model and update other models too

--- main/local_settings.py
+++
@@ -1,8 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d$ds((7fnjyxp)=76xdf4$j9&xah512+w)y%*3_igq=4&-9a*='
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
