a5ba8c4617145fb69a06a8b632c44bfd7ce6845d
mamarezak22 <mohammadreza13842018@gmail.com>
2025-03-09 16:24:10 +0330

    registering models on admin.py

--- questions/admin.py
+++ questions/admin.py
@@ -7,11 +7,14 @@ from questions.models import Question, Answer, Category
 class AnswerInlineAdmin(admin.TabularInline):
     model = Answer
     extra = 1
+
+@admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     class Meta:
         model = Question
         inlines = [AnswerInlineAdmin]
         filter_horizontal = ('category',)
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     class Meta:
         model = Category
