b2d681beaceccb0e96cfa2a5fb509beb49b07cac
mamarezak22 <mohammadreza13842018@gmail.com>
2025-03-09 16:20:01 +0330

    adding admin page

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from questions.models import Question, Answer, Category
+
+
 # Register your models here.
+class AnswerInlineAdmin(admin.TabularInline):
+    model = Answer
+    extra = 1
+class QuestionAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Question
+        inlines = [AnswerInlineAdmin]
+        filter_horizontal = ('category',)
+class CategoryAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Category
